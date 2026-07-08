bacc58d85f526d4ddb2424828ea93091e445f0e8
Gaybg13 <107002835+Gaybg13@users.noreply.github.com>
2026-07-08 10:08:08 +0200

    Update Firebase configuration with new credentials

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -1,10 +1,10 @@
 self.__FIREBASE_CONFIG__ = {
-  apiKey: "REMPLACER_API_KEY",
-  authDomain: "REMPLACER_AUTH_DOMAIN",
-  projectId: "REMPLACER_PROJECT_ID",
-  storageBucket: "REMPLACER_STORAGE_BUCKET",
-  messagingSenderId: "REMPLACER_MESSAGING_SENDER_ID",
-  appId: "REMPLACER_WEB_APP_ID"
+  apiKey: "AIzaSyC77mFQcQY2TgMmdwM826-aCQ8_8NMi0lkY",
+  authDomain: "chorale-lumina-app.firebaseapp.com",
+  projectId: "chorale-lumina-app",
+  storageBucket: "chorale-lumina-app.firebasestorage.app",
+  messagingSenderId: "629726175588",
+  appId: "1:629726175588:web:893837be6ead3d42715a71"
 };
 
-self.__FIREBASE_VAPID_KEY__ = "REMPLACER_CLE_VAPID_PUBLIQUE";
+self.__FIREBASE_VAPID_KEY__ = "BC1mD_OTMHzhYfq3jfSuMihrVsqXKp7nrHh1Py9ndZsZ9E5MVYbX9vJyVvj0lJhZuey5ySnGABibsaHv3wUCkxI";
